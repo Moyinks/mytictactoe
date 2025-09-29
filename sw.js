0a8ab9739815b758c3151e1b68ce67ab32447b8c
Moyinks <moyinksvibes@gmail.com>
2025-09-29 12:42:55 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("tictactoe-v1").then(cache => cache.addAll([
+      "index.html",
+      "manifest.webmanifest",
+      "background7.jpg",
+      "xSound.mp3",
+      "oSound.mp3",
+      "win.mp3",
+      "pwa192.png",
+      "pwa512.png"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
